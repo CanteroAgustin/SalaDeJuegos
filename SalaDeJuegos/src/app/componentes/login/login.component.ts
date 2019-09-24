383cdaa071b542a03ce2b3191e5143e7b338ed72
Agustin Cantero <agustincantero11@gmail.com>
2019-09-24 10:19:45 -0300

    modificaciones login

--- SalaDeJuegos/src/app/componentes/login/login.component.ts
+++ SalaDeJuegos/src/app/componentes/login/login.component.ts
@@ -4,6 +4,7 @@ import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import { Subscription } from "rxjs";
 import { TimerObservable } from "rxjs/observable/TimerObservable";
 import { UsuarioService } from 'src/app/servicios/usuario.service';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -13,28 +14,30 @@ import { UsuarioService } from 'src/app/servicios/usuario.service';
 export class LoginComponent implements OnInit {
 
   private subscription: Subscription;
-  usuario = '';
-  clave = '';
   progreso: number;
   progresoMensaje = "esperando...";
   logeando = true;
   ProgresoDeAncho: string;
+  loginForm;
 
   clase = "progress-bar progress-bar-info progress-bar-striped ";
 
   constructor(
-    private route: ActivatedRoute,
-    private router: Router, private usuarioService: UsuarioService) {
+    private router: Router, private usuarioService: UsuarioService,
+    private formBuilder: FormBuilder) {
     this.progreso = 0;
     this.ProgresoDeAncho = "0%";
-
+    this.loginForm = this.formBuilder.group({
+      usuario: '',
+      clave: ''
+    });
   }
 
   ngOnInit() {
   }
 
-  Entrar() {
-    this.usuarioService.loggear(this.usuario, this.clave).then(data => {
+  Entrar(data) {
+    this.usuarioService.loggear(data.usuario, data.clave).then(data => {
       this.router.navigate(['/Principal']);
     }).catch(error => {
       console.log(error);
@@ -42,7 +45,7 @@ export class LoginComponent implements OnInit {
   }
 
 
-  MoverBarraDeProgreso() {
+  MoverBarraDeProgreso(data) {
 
     this.logeando = false;
     this.clase = "progress-bar progress-bar-danger progress-bar-striped active";
@@ -54,7 +57,7 @@ export class LoginComponent implements OnInit {
       this.ProgresoDeAncho = this.progreso + 20 + "%";
       if (this.progreso == 100) {
         this.subscription.unsubscribe();
-        this.Entrar();
+        this.Entrar(data);
       }
     });
     //this.logeando=true;
